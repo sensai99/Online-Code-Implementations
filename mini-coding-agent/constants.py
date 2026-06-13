a5df46158962bfe11ace1c7907c92a2130f90423
Sai Sreenivas <chinthasaisreenivas@gmail.com>
2026-06-13 00:42:47 -0400

    fix constants.py

--- mini-coding-agent/constants.py
+++ mini-coding-agent/constants.py
@@ -1,14 +1,10 @@
 from utils.file_utils import set_env_vars
 
+MAX_RETRIES = 3
+
 env_vars = set_env_vars('env.txt')
 OPENAI_API_KEY = env_vars['OPENAI_API_KEY']
 GOOGLE_API_KEY = env_vars['GOOGLE_API_KEY']
 AZURE_API_KEY = env_vars['AZURE_API_KEY']
 OPENAI_AZURE_BASE_URL = env_vars['OPENAI_AZURE_BASE_URL']
 OPENAI_OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
-
-print(OPENAI_OPENROUTER_BASE_URL)
-print(OPENAI_API_KEY)
-print(GOOGLE_API_KEY)
-print(AZURE_API_KEY)
-print(OPENAI_AZURE_BASE_URL)
