e293f461df009b965a6e454878f95ca3d02b9c1f
Sai Sreenivas <chinthasaisreenivas@gmail.com>
2026-06-13 00:43:07 -0400

    add retry mechanism

--- mini-coding-agent/run.py
+++ mini-coding-agent/run.py
@@ -14,16 +14,6 @@ def build_prompt(context):
     system_prompt = SYSTEM_PROMPT + INSTRUCTION_PROMPT + TOOLS_PROMPT + CONTEXT_PROMPT + "\n\nContext: " + json.dumps(context)
     return system_prompt
 
-def extract_tools_object(tools_response):
-    try:
-        blocks = re.search(r'<answer>(.*)</answer>', tools_response, re.DOTALL).group(1)
-        return json.loads(blocks)
-    except Exception as e:
-        raise ValueError(f"Error extracting tools object: {json.dumps({
-            "tools_response": tools_response,
-            "error": str(e)
-        })}")
-
 def run(context):
 
     # new context of the conversation
@@ -37,19 +27,17 @@ def run(context):
     system_prompt = build_prompt(context)
     user_prompt = context[-1]["content"] # last user message is the user query
     
-    # call the LLM to determine the tools to use
-    tools_response = llm_manager.call_llm(system_prompt, user_prompt, model="gpt-4.1-mini")
-
-    # convert the tools response to a JSON object
-    tools_object = extract_tools_object(tools_response)
+    # call the LLM to determine the tools to use 
+    # get the response object from the LLM
+    response_object, errors = llm_manager.get_response_object(system_prompt, user_prompt, model="gpt-4.1-mini")
     
     # print the initial response from the LLM
-    llm_response = tools_object['message']
-    new_context.append({"role": "assistant", "content": llm_response})
+    llm_response = response_object['message']
+    new_context.append({"role": "assistant", "content": {"response": llm_response, "actions": response_object['actions'], "errors": errors}})
     print(llm_response)
     
     # approval + apply the tools
-    for tool_object in tools_object['actions']:
+    for tool_object in response_object['actions']:
         tool_name = tool_object['tool']
         parameters = {k: v for k, v in tool_object.items() if k != 'tool'}
         result = tools_manager(tool_name, **parameters)
